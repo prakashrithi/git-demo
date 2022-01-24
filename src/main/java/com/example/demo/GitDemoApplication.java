f4f3bf39a455a2ab9c5af7c8e5aa094bdbd0a49b
prakashrithi <ailaprakash96@gmail.com>
2022-01-24 14:42:18 +0530

    second commit with github

--- src/main/java/com/example/demo/GitDemoApplication.java
+++ src/main/java/com/example/demo/GitDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitDemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("testing");
 		SpringApplication.run(GitDemoApplication.class, args);
 	}
 
